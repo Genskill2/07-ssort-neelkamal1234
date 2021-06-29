79184f8f3b430bca808a0870c31245dbd096445b
neelkamal1234 <84660248+neelkamal1234@users.noreply.github.com>
2021-06-29 22:10:23 +0530

    NC5

--- swap.c
+++ swap.c
@@ -5,11 +5,12 @@ void swap_max( int arr[],int l, int n)
   for( int i=n;i<l;i++)
   {    if(arr[i]>max)
         { max=arr[i];
-          pos=i;
+          pos=i;}
+  }
           arr[pos]=arr[n];
           arr[n]=max;
       }
- }
+ 
  
 }
  
