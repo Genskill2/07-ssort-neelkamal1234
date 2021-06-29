d97e05dd7a943fdb788751f915a9023d09653982
neelkamal1234 <84660248+neelkamal1234@users.noreply.github.com>
2021-06-29 21:52:35 +0530

    NC1

---
+++ swap.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <cs50.h>
+int swap_max( int arr[],int l, int n)
+{ int max=arr[n]; intpos =n;
+  for( int i=n;i<l;i++)
+  { if(arr[i]>max)
+     { max=arr[i;
+       pos=i;
+       arr[pos]=arr[n];
+        arr[n]=max;
+      }
+ }
+ 
+ int ssort(int arr[],int l)
+ { for(int i=o;i<l;i++)
+   swap_max(arr,l,i);
+   }
+   printf("Sorted Array \n");
+   for(int i=0;i<l;i++)
+      printf("%i| \n",arr[i]);
+  }
